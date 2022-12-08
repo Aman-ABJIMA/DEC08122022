7dc4ae9e154070208d8ce61beb4c99dc7b13290d
Aman-ABJIMA <119652332+Aman-ABJIMA@users.noreply.github.com>
2022-12-08 18:21:11 +0530

    Add files via upload

---
+++ StructureBasedProgram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Structure
+{
+    class Program
+    {
+        struct Employee
+        {
+            public int id;
+            public string name;
+
+            public void function1(int id)
+            {
+                Console.WriteLine("Employee Id:" + id);
+            }
+            public void function2(string name)
+            {
+                Console.WriteLine("Employee NAME:" + name);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+            Employee emp;
+
+            Console.WriteLine("Enter Employee ID:");
+
+            emp.id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Name:");
+
+            emp.name = Console.ReadLine();
+
+
+            emp.function1(emp.id);
+            emp.function2(emp.name);
+
+        }
+
+
+    
+
+   
+    }
+}
